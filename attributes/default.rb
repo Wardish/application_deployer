85de76140dec19c74b8a9ab11b2bde596ef1f251
Tetsuya Mito <tetsuya@wardish.jp>
2017-08-15 18:46:26 +0900

    add default attributes.

---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+
+default['application']['db']['name'] = 'sample_db'
+default['applictaion']['db']['password'] = 'sample_pass'
+default['applictaion']['db']['host'] = 'localhost'
+default['application']['dir'] = '/vagrant/modules/codeigniter'
+default['application']['name'] = 'sample_app'
+default['application']['auth']['require'] = false
+default['application']['auth']['name'] = 'Message on Basic Authentication dialog'
+default['application']['auth']['db']['table'] = 'tb_user'
+default['application']['auth']['db']['id_field'] = 'user_id'
+default['application']['auth']['db']['id_field'] = 'passowrd'
