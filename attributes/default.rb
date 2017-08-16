70c5b25935635acbe74b86d74062506e47d2b07b
Tetsuya Mito <tetsuya@wardish.jp>
2017-08-16 18:25:55 +0900

    add a public folder specification.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@ default['application']['db']['name'] = 'sample_db'
 default['applictaion']['db']['password'] = 'sample_pass'
 default['applictaion']['db']['host'] = 'localhost'
 default['application']['dir'] = '/vagrant/modules/codeigniter'
+default['application']['dir']['public'] = default['application']['dir'] + "/public"
 default['application']['name'] = 'sample_app'
 default['application']['auth']['require'] = false
 default['application']['auth']['name'] = 'Message on Basic Authentication dialog'
